1fc71e0001700256da7e8b717e0d388263079171
Celestial <116771638+celestial-official@users.noreply.github.com>
2024-02-16 02:47:26 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+function returnData() {
+	return fetch("https://api.ipify.org?format=json").then(response => response.json()).then(data => {
+		return JSON.stringify(data);
+	}).catch(error => {
+		return JSON.strigify({error: "Couldn't fetch requested data."});
+	});
+}
