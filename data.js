9c7700be4c124e078b62ffc140ca4d222b588d2a
Celestial <116771638+celestial-official@users.noreply.github.com>
2024-02-16 02:50:32 +0100

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-function returnData() {
-	return fetch("https://api.ipify.org?format=json").then(response => response.json()).then(data => {
-		return JSON.stringify(data);
-	}).catch(error => {
-		return JSON.strigify({error: "Couldn't fetch requested data."});
-	});
-}
-
-returnData();
